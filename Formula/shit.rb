c0603ea3627cf9edc6758b485ba09059afc2e487
Ezra Natanael <ezrantn@proton.me>
2024-12-23 16:39:52 +0700

    fix sha

--- Formula/shit.rb
+++ Formula/shit.rb
@@ -2,7 +2,7 @@ class Shit < Formula
     desc "A simple CLI tool to kill processes by name"
     homepage "https://github.com/ezrantn/shit"
     url "https://github.com/ezrantn/shit/archive/refs/tags/v1.0.0.tar.gz"
-    sha256 "d21393e9f6c8bbc26d187cd325be171c8081105c42862f3e5845dc33a62a045a"
+    sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
     head "https://github.com/ezrantn/shit.git", branch: "main"
 
     depends_on "go" => :build
